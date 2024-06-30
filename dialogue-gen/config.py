7a2c1abb9fb0fa991cff27c33d3bc49d89bb00ae
RickyRister <ricky.rister.wang@gmail.com>
2024-06-29 23:09:59 -0700

    load configs and cli args

---
+++ dialogue-gen/config.py
@@ -0,0 +1,9 @@
+from typing import Union
+from argparse import Namespace
+
+# parsed command line args
+ARGS: Namespace
+
+# loaded from the json config on startup
+JSON_CONFIG: dict
+
---
+++ dialogue-gen/dialogue-gen.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+import json
+import config
+
+
+def createArgumentParser() -> ArgumentParser:
+    parser = ArgumentParser(description='Generate .')
+    parser.add_argument('--configFile', type=str,
+                        help='path to the config file', default='dialogue-gen.json')
+    parser.add_argument('--outFile',  type=str,
+                        help='base name of the output file', default='output.mlt')
+    return parser
+
+
+def main():
+    parser = createArgumentParser()
+    config.ARGS = parser.parse_args()
+
+    with open(config.ARGS.configFile) as configFile:
+        config.JSON_CONFIG = json.load(configFile)
+    
+
+
+if __name__ == "__main__":
+    main()
