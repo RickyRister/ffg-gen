dd39d91cf9b48c6ebf1dbca330a2d5fe311f3215
RickyRister <ricky.rister.wang@gmail.com>
2024-07-02 21:42:31 -0700

    move duration classes to own file

--- ffg-gen/configs.py
+++ ffg-gen/configs.py
@@ -1,6 +1,7 @@
 from argparse import Namespace
 from dataclasses import dataclass
 import json
+from duration import DurationFix, Threshold
 
 # parsed command line args
 ARGS: Namespace
@@ -39,27 +40,6 @@ class DialogueBoxConfigs:
     dropTextEnd: str
     font: str
     fontSize: int
-
-
-@dataclass
-class DurationFix:
-    """Contains info about converting frame durations to timestamp durations.
-    The expected out frame won't be known at first.
-    We recommend you run the tool first to fill in the expected frame and the correct fix.
-    """
-
-    expectedFrames: int     # expected out frame
-    fix: str                # timestamp fix
-    seconds: str = None     # optional field to leave comment
-
-
-@dataclass
-class Threshold:
-    """Contains info about mapping count to duration
-    """
-
-    count: int
-    seconds: float
 
 
 @dataclass
---
+++ ffg-gen/duration.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+@dataclass
+class DurationFix:
+    """Contains info about converting frame durations to timestamp durations.
+    The expected out frame won't be known at first.
+    We recommend you run the tool first to fill in the expected frame and the correct fix.
+    """
+
+    expectedFrames: int     # expected out frame
+    fix: str                # timestamp fix
+    seconds: str = None     # optional field to leave comment
+
+
+@dataclass
+class Threshold:
+    """Contains info about mapping count to duration
+    """
+
+    count: int
+    seconds: float
