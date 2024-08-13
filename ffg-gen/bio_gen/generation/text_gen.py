cbce6393c05496b86ad5b7500b01cac11009c88c
RickyRister <ricky.rister.wang@gmail.com>
2024-08-12 23:29:19 -0700

    text_gen now respects first and last fade durations

--- ffg-gen/bio_gen/generation/text_gen.py
+++ ffg-gen/bio_gen/generation/text_gen.py
@@ -1,23 +1,23 @@
+from enum import Enum
+from typing import Generator
 from vidpy import Clip
+from dataclasses import dataclass
 from vidpy.utils import Frame
 import filters
-from bio_gen.bioline import Line
+from bio_gen.bioline import Line, BioTextBlock
 from bio_gen.bioinfo import BioInfo
 from bio_gen.sysline import SysLine
 from vidpy_extension.ext_composition import ExtComposition
 import configs
-import durations
 from exceptions import expect
 
 
-def filter_none(lines: list) -> list:
-    return [line for line in lines if line is not None]
-
+# === Entrance ====
 
 def generate(lines: list[Line]) -> ExtComposition:
     """Processes the list of lines into a Composition
     """
-    clips: list[Clip] = filter_none([line_to_clip(line) for line in lines])
+    clips: list[Clip] = list(process_lines(lines))
 
     return ExtComposition(
         clips,
@@ -27,18 +27,30 @@ def generate(lines: list[Line]) -> ExtComposition:
         fps=configs.VIDEO_MODE.fps)
 
 
-def line_to_clip(line: Line) -> Clip | None:
-    if isinstance(line, SysLine):
-        # always run the pre_hook first if it's a sysline
-        line.pre_hook()
+# === Processing Lines ===
 
-        # match sysline (we currently don't support Wait yet)
-        match line:
-            case _: return None
+def process_lines(lines: list[Line]) -> Generator[Clip, None, None]:
+    for index, line in enumerate(lines):
+        if isinstance(line, SysLine):
+            # always run the pre_hook first if it's a sysline
+            line.pre_hook()
 
-    # grab the appropriate info
+            # match sysline (we currently don't support Wait yet)
+            match line:
+                case _: ...
+        else:
+            # figure out if the clip is on either boundary
+            is_first = True if index == 0 else False
+            is_last = True if index == len(lines) - 1 else False
+
+            # create the clip
+            yield line_to_clip(line, is_first, is_last)
+
+
+def line_to_clip(line: BioTextBlock, is_first: bool, is_last: bool) -> Clip:
     bioInfo: BioInfo = BioInfo.of_name(line.name)
 
+    # text filters
     richTextFilter: dict = filters.richTextFilterArgs(
         text=line.text,
         geometry=expect(bioInfo.bioGeometry, 'bioGeometry'),
@@ -46,10 +58,15 @@ def line_to_clip(line: Line) -> Clip | None:
         fontSize=expect(bioInfo.bioFontSize, 'bioFontSize'),
         color=expect(bioInfo.bioFontColor, 'bioFontColor'))
 
-    fadeInEnd = expect(bioInfo.textFadeInEnd, 'textFadeInEnd')
-    fadeOutDur = expect(bioInfo.textFadeOutDur, 'textFadeOutDur')
+    # figure out fade filters
+    fadeInEnd = expect(bioInfo.enterFadeInEnd, 'enterFadeInEnd') if is_first\
+        else expect(bioInfo.textFadeInEnd, 'textFadeInEnd')
+
+    fadeOutDur = expect(bioInfo.exitFadeOutDur, 'exitFadeOutDur') if is_last\
+        else expect(bioInfo.textFadeOutDur, 'textFadeOutDur')
     fadeOutStart = line.duration - fadeOutDur
 
+    # create clip
     return Clip('color:#00000000', start=Frame(0)).set_duration(line.duration)\
         .fx('qtext', richTextFilter)\
         .fx('brightness', filters.opacityFilterArgs(f'0=0;{fadeInEnd}=1'))\
