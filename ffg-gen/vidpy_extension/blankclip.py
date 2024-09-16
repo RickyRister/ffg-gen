7da36e509ad823454289ed805ba512bfee88ad2c
RickyRister <ricky.rister.wang@gmail.com>
2024-09-15 17:45:07 -0700

    explicitly convert duration to Frames in BlankClip

--- ffg-gen/vidpy_extension/blankclip.py
+++ ffg-gen/vidpy_extension/blankclip.py
@@ -15,10 +15,12 @@ class BlankClip(Clip):
     Don't use unless you're certain that all gap lengths are covered by duration fixes.
     '''
 
-    def ofDuration(duration: Second | Frame):
-        '''Creates a blank clip of the given duration
+    def ofDuration(duration: Frame):
+        '''Creates a blank clip of the given duration.
+        Only accepts Frames as duration.
+        Will cast the duration to a Frame just to be double sure that it doesn't get interpreted as a Second
         '''
-        return BlankClip(start=Frame(0)).set_offset(duration)
+        return BlankClip(start=Frame(0)).set_offset(Frame(duration))
 
     def args(self, singletrack=False):
         '''Returns melt command line arguments as a list'''
@@ -34,11 +36,11 @@ class BlankClip(Clip):
         return args
 
 
-def transparent_clip(duration: Second | Frame) -> Clip:
+def transparent_clip(duration: Frame) -> Clip:
     '''Creates a blank clip with the given duration.
     If the option --fill-blanks is on, this will instead return a transparent clip with the given duration 
     '''
     if cli_args.ARGS.fill_blanks:
-        return Clip('color:#00000000', start=Frame(0)).set_duration(duration)
+        return Clip('color:#00000000', start=Frame(0)).set_duration(Frame(duration))
     else:
         return BlankClip.ofDuration(duration)
